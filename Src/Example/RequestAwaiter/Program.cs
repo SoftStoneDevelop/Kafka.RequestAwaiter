d5ea9b4e3ff6fe8d886f8ae37163e548c176777a
Vyacheslav <softstonedevelop@gmail.com>
2023-07-29 20:04:33 +0300

    feat: + test console app

---
+++ Src/Example/RequestAwaiter/Program.cs
@@ -0,0 +1,112 @@
+﻿using Confluent.Kafka;
+using KafkaExchanger.Attributes;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RequestAwaiterConsole
+{
+    [RequestAwaiter(useLogger: false),
+        Income(keyType: typeof(Null), valueType: typeof(string)),
+        Income(keyType: typeof(Null), valueType: typeof(string)),
+        Outcome(keyType: typeof(Null), valueType: typeof(string))
+        ]
+    public partial class RequestAwaiter
+    {
+
+    }
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bootstrapServers = "localhost:9194, localhost:9294, localhost:9394";
+            var input0Name = "RAInputSimple1";
+            var input1Name = "RAInputSimple2";
+            var outputName = "RAOutputSimple";
+
+            var responderName0 = "RAResponder1";
+            var responderName1 = "RAResponder2";
+
+            var pool = new KafkaExchanger.Common.ProducerPoolNullString(5, bootstrapServers);
+            using var reqAwaiter = new RequestAwaiter();
+            var reqAwaiterConfitg =
+                new RequestAwaiter.Config(
+                    groupId: "SimpleProduce",
+                    bootstrapServers: bootstrapServers,
+                    processors: new RequestAwaiter.ProcessorConfig[]
+                    {
+                        //From _inputSimpleTopic1
+                        new RequestAwaiter.ProcessorConfig(
+                            income0: new RequestAwaiter.ConsumerInfo(
+                                topicName: input0Name,
+                                canAnswerService: new [] { responderName0 },
+                                partitions: new int[] { 0 }
+                                ),
+                            income1: new RequestAwaiter.ConsumerInfo(
+                                topicName: input1Name,
+                                canAnswerService: new [] { responderName1 },
+                                partitions: new int[] { 0 }
+                                ),
+                            new RequestAwaiter.ProducerInfo(outputName),
+                            buckets: 2,
+                            maxInFly: 50
+                            ),
+                        new RequestAwaiter.ProcessorConfig(
+                            income0: new RequestAwaiter.ConsumerInfo(
+                                topicName: input0Name,
+                                canAnswerService: new [] { responderName0 },
+                                partitions: new int[] { 1 }
+                                ),
+                            income1: new RequestAwaiter.ConsumerInfo(
+                                topicName: input1Name,
+                                canAnswerService: new [] { responderName1 },
+                                partitions: new int[] { 1 }
+                                ),
+                            new RequestAwaiter.ProducerInfo(outputName),
+                            buckets: 2,
+                            maxInFly: 50
+                            ),
+                        new RequestAwaiter.ProcessorConfig(
+                            income0: new RequestAwaiter.ConsumerInfo(
+                                topicName: input0Name,
+                                canAnswerService: new [] { responderName0 },
+                                partitions: new int[] { 2 }
+                                ),
+                            income1: new RequestAwaiter.ConsumerInfo(
+                                topicName: input1Name,
+                                canAnswerService: new [] { responderName1 },
+                                partitions: new int[] { 2 }
+                                ),
+                            new RequestAwaiter.ProducerInfo(outputName),
+                            buckets: 2,
+                            maxInFly: 50
+                            )
+                    }
+                    );
+            reqAwaiter.Start(reqAwaiterConfitg, producerPool0: pool);
+
+            Stopwatch sb = Stopwatch.StartNew();
+            for (int i = 0; i < 1000; i++)
+            {
+                var result = await reqAwaiter.Produce("Hello");
+                Console.WriteLine("Result " + i + ":");
+                Console.WriteLine(((ResponseItem<RequestAwaiter.Income0Message>)result.Result[0]).Result.Value);
+                Console.WriteLine(((ResponseItem<RequestAwaiter.Income1Message>)result.Result[1]).Result.Value);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            sb.Stop();
+            Console.WriteLine($"Time: {sb.Elapsed}");
+
+            while ( true )
+            {
+                var read = Console.ReadLine();
+                if(read == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Src/Example/Responder1Console/Program.cs
@@ -0,0 +1,64 @@
+﻿using Confluent.Kafka;
+using KafkaExchanger.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace Responder0Console
+{
+    [Responder(useLogger: false),
+        Income(keyType: typeof(Null), valueType: typeof(string)),
+        Outcome(keyType: typeof(Null), valueType: typeof(string))
+        ]
+    public partial class ResponderOneToOneSimple
+    {
+
+    }
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bootstrapServers = "localhost:9194, localhost:9294, localhost:9394";
+            var inputName = "RAOutputSimple";
+            var responderName = "RAResponder2";
+
+            var responder2 = new ResponderOneToOneSimple();
+            var responder1Config =
+                new ResponderOneToOneSimple.ConfigResponder(
+                groupId: responderName,
+                serviceName: responderName,
+                bootstrapServers: bootstrapServers,
+                new ResponderOneToOneSimple.ConsumerResponderConfig[]
+                {
+                    new ResponderOneToOneSimple.ConsumerResponderConfig(
+                        createAnswer: (input, s) =>
+                {
+                    var result = new ResponderOneToOneSimple.OutcomeMessage()
+                    {
+                        Value = $"2: Answer {input.Value}"
+                    };
+
+                    return Task.FromResult(result);
+                },
+                        incomeTopicName: inputName,
+                        partitions: new int[] { 0, 1, 2 }
+                        )
+                }
+                );
+
+            var pool = new KafkaExchanger.Common.ProducerPoolNullString(5, bootstrapServers);
+            responder2.Start(config: responder1Config, producerPool: pool);
+
+            while (true)
+            {
+                var read = Console.ReadLine();
+                if (read == "exit")
+                {
+                    break;
+                }
+            }
+
+            await responder2.StopAsync();
+        }
+    }
+}
